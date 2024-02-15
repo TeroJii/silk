15ac272fa4c434a504c6c5619dc4b201e02a4da9
TeroJii <78660782+TeroJii@users.noreply.github.com>
2024-02-15 14:59:46 +0200

    add skeleton for first function

---
+++ R/find_outliers.R
@@ -0,0 +1,12 @@
+#' Find outliers in a dataset
+#'
+#' @param dat A data.frame with the data
+#' @param col The column to find outliers from
+#'
+#' @return
+#' @export
+#'
+#' @examples
+find_outliers <- function(dat, col) {
+  return(dat)
+}
