6a8aecb44fb7c25bc5ab537d83c8f3616cf49c72
TeroJii <78660782+TeroJii@users.noreply.github.com>
2024-02-15 15:04:57 +0200

    add placeholders to avoid document() warnings

--- R/find_outliers.R
+++ R/find_outliers.R
@@ -3,10 +3,11 @@
 #' @param dat A data.frame with the data
 #' @param col The column to find outliers from
 #'
-#' @return
+#' @return A data.frame with something
 #' @export
 #'
 #' @examples
+#'  # Examples here
 find_outliers <- function(dat, col) {
   return(dat)
 }
