3785a29c991c21df3b30260183698d7c9b615480
TeroJii <78660782+TeroJii@users.noreply.github.com>
2024-02-16 13:42:29 +0200

    add internal helper function for recording median values

--- R/find_outliers.R
+++ R/find_outliers.R
@@ -2,12 +2,42 @@
 #'
 #' @param x A data.frame with the data
 #' @param col The column to find outliers from
+#' @param group_col The column to group by
 #'
 #' @return A data.frame with something
 #' @export
 #'
 #' @examples
 #'  # Examples here
-find_outliers <- function(x, col) {
+find_outliers <- function(x, col, group_col = NULL) {
+
+  stopifnot(is.data.frame(x))
+  stopifnot(is.character(col))
+  stopifnot(is.null(group_col) || is.character(group_col))
+
   return(x)
 }
+
+
+
+
+record_median <- function(x, col, group_col){
+
+  stopifnot(is.data.frame(x))
+  stopifnot(is.character(col))
+  stopifnot(is.character(group_col))
+
+  medians <- x |>
+    dplyr::group_by(.data[[group_var]]) |>
+    dplyr::summarise(
+      median_speed = stats::median(.data[[col]], na.rm = TRUE),
+      mad = stats::mad(.data[[col]], na.rm = TRUE)
+    ) |>
+    dplyr::ungroup()
+
+  return(medians)
+}
+
+
+# suppress undefined global functions or variables during R CMD check
+.data <- group_var <- NULL
