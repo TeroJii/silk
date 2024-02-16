d861c8e10177ace5f184d68f0fbcd82b31b6f8bc
TeroJii <78660782+TeroJii@users.noreply.github.com>
2024-02-16 15:38:23 +0200

    test that expected columns are returned

--- tests/testthat/test-find_outliers.R
+++ tests/testthat/test-find_outliers.R
@@ -1,4 +1,4 @@
-df <- data.frame(x = 1:10, y = "x")
+df <- data.frame(x = 1:10, y = "val1")
 
 # A data.frame is returned
 test_that("data.frame as a return value", {
@@ -7,4 +7,11 @@ test_that("data.frame as a return value", {
 
 test_that("invalid group_col throws an error", {
   expect_error(find_outliers(x = df, col = "x", group_col = "col_not_in_df"))
+})
+
+test_that("returned data.frame contains column .median, .mad and .outlier", {
+  testthat::expect_contains(
+    object = names(find_outliers(x = df, col = "x", group_col = "y")),
+    expected = c(".median", ".mad", ".outlier")
+  )
 })
