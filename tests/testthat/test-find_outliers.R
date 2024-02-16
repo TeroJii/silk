c79bea4095c07e9d448f274220a07e9544774e33
TeroJii <78660782+TeroJii@users.noreply.github.com>
2024-02-16 13:48:18 +0200

    test that invalid group_col results in error

--- tests/testthat/test-find_outliers.R
+++ tests/testthat/test-find_outliers.R
@@ -4,3 +4,7 @@ df <- data.frame(x = 1:10, y = "x")
 test_that("data.frame as a return value", {
   expect_s3_class(find_outliers(x = df, col = "x"), "data.frame")
 })
+
+test_that("invalid group_col throws an error", {
+  expect_error(find_outliers(x = df, col = "x", group_col = "col_not_in_df"))
+})
