71baa33b74afe378ae7ab78c28769bb06beef37a
TeroJii <78660782+TeroJii@users.noreply.github.com>
2024-02-16 13:43:00 +0200

    update test to reflect new parameter names

--- tests/testthat/test-find_outliers.R
+++ tests/testthat/test-find_outliers.R
@@ -2,5 +2,5 @@ df <- data.frame(x = 1:10, y = "x")
 
 # A data.frame is returned
 test_that("data.frame as a return value", {
-  expect_s3_class(find_outliers(x = df), "data.frame")
+  expect_s3_class(find_outliers(x = df, col = "x"), "data.frame")
 })
