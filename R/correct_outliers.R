5c80f9b5109e381d56e9b0c982c19761b73d02a8
TeroJii <78660782+TeroJii@users.noreply.github.com>
2024-02-19 11:13:29 +0200

    add basic implementation for correct_outliers()

--- R/correct_outliers.R
+++ R/correct_outliers.R
@@ -1,3 +1,16 @@
-correct_outliers <- function(){
+correct_outliers <- function(x, col){
 
+  stopifnot(is.data.frame(x))
+
+  if(!any(names(x) %in% ".outlier")){
+    stop("No .outlier column found in the data. Please run find_outliers() on your data.frame first.")
+  }
+
+  x |>
+    dplyr::mutate("{{col}}" := dplyr::if_else(
+      .outlier,
+      true = (dplyr::lag(.data[[col]]) + dplyr::lead(.data[[col]]))/2,
+      false = .data[[col]]
+    )
+    )
 }
