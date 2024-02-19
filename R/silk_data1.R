3189e0df57b8d00264957bb81e9b345cc1aa9ef9
TeroJii <78660782+TeroJii@users.noreply.github.com>
2024-02-19 15:26:02 +0200

    document example data

---
+++ R/silk_data1.R
@@ -0,0 +1,16 @@
+#' Example data for the silk package.
+#'
+#' This dataset is used to demonstrate the functionality of the silk package.
+#' Data contains two series of data, each with 1000 observations. Two outliers
+#' are present in each series.
+#'
+#' @format ## `silk_data1`
+#' A data frame with 2,000 rows and 3 columns:
+#' \describe{
+#'   \item{time}{x-axis variable, such as time}
+#'   \item{y}{Response variable}
+#'   \item{group}{Grouping variable for the two series}
+#'   ...
+#' }
+#' @source silk package
+"silk_data1"
