c8c31c5d12a86c172c7c56dab84e8b269b10a0d5
TeroJii <78660782+TeroJii@users.noreply.github.com>
2024-02-20 10:27:10 +0200

    add test to see that outliers in example data are corrected as expected

--- tests/testthat/test-correct_outliers.R
+++ tests/testthat/test-correct_outliers.R
@@ -4,3 +4,16 @@ df <- data.frame(x = 1:10, y = "val1")
 test_that("outlier correction throws an error due to missing .outlier-column", {
   expect_error(correct_outliers(x = df, col = "x"))
 })
+
+test_that("Corrected example data has a smaller mean than raw data with outliers", {
+  expect_true(
+    object = {
+      corrected_dat <- silk_data1 |>
+        find_outliers("y", group_col = "group") |>
+        correct_outliers(col = "y")
+
+
+      mean(corrected_dat$y) < mean(silk_data1$y)
+    }
+  )
+})
